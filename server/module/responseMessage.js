622e49327e95150c357a2a3d93ced0cc3a262b60
siyeons <siyeonleeme@gmail.com>
2019-11-16 05:52:28 +0900

    create responseMessage module

---
+++ server/module/responseMessage.js
@@ -0,0 +1,10 @@
+// responseMessage.js
+module.exports = {
+    NULL_VALUE: "필요한 값이 없습니다.",
+    OUT_OF_VALUE: "파라미터 값이 잘못 되었습니다.",
+
+    FACE_READ_SUCCESS: "퀴즈 조회 성공",
+    FACE_READ_FAIL: "퀴즈 조회 성공",
+
+    INTERNAL_SERVER_ERROR: "서버 내부 오류",
+}
