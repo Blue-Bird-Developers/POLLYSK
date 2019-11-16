be7394b0d0f1ecaff803636799e4c79a18a79d33
siyeons <siyeonleeme@gmail.com>
2019-11-16 11:30:53 +0900

    rekognition 구동 완료

--- server/routes/api/rekognition/rekognition.js
+++ server/routes/api/rekognition/rekognition.js
@@ -11,7 +11,7 @@ const axios = require('axios')
 const rekognition = new AWS.Rekognition();
 
 router.post('/:userId', (req, res) => {
-    const userId = req.paramss.userId;
+    const userId = req.params.userId;
     var faceSingle = '';
 
     if(!userId){
@@ -40,55 +40,42 @@ router.post('/:userId', (req, res) => {
     //     res.status(code).send(json)
     // });
 
-    // const params = {
-    //     "Image": {
-    //         "Bytes": faceSingle,
-    //     },
-    //     "Attributes": ["ALL"]
-    // }
-    console.log(faceSingle);
-//     const imageURL = req.query.imageURL
-//   // 이미지 URL로부터 해당 이미지를 arraybuffer 형식으로 가져옵니다.
-//     const responseImage = await axios.get(
-//         imageURL,
-//         { responseType: 'arraybuffer' }
-//     )
 
-//   // rekognition의 detectText 함수에 필요한 parameter 변수입니다.
-//     const params = {
-//         Image: {
-//             Bytes: new Buffer(responseImage.data, 'binary')
-//         }
-//     }
+  // rekognition의 detectText 함수에 필요한 parameter 변수입니다.
+    const params = {
+        Image: {
+            Bytes: faceSingle
+        }
+    }
 
-//     rekognition.detectFaces(params, (error, data) => {
-//         if(error) {
-//             res.send(error);
-//         }
-//         else {
-//             if(data.FaceDetails && data.FaceDetails.length > 0) {
-//                 const detectedAge = {
-//                     age : detectUserAgeRange(data)
-//                 };
-//                 res.json(detectedAge);
-//             }
-//             else {
-//                 const invalidAge = {
-//                     age : "none"
-//                 };
-//                 res.json(invalidAge);
-//             }
-//         }
-//     });
- });
+    rekognition.detectFaces(params, (error, data) => {
+        if(error) {
+            res.send(error);
+        }
+        else {
+            if(data.FaceDetails && data.FaceDetails.length > 0) {
+                const detectedAge = {
+                    age : detectUserAgeRange(data)
+                };
+                res.json(detectedAge);
+            }
+            else {
+                const invalidAge = {
+                    age : "none"
+                };
+                res.json(invalidAge);
+            }
+        }
+    });
+});
 
-// const detectUserAgeRange = (data) => {
-//     const ageRangeFromFace = data.FaceDetails[0].AgeRange;
-//     const low = ageRangeFromFace.Low;
-//     const high = ageRangeFromFace.High;
+const detectUserAgeRange = (data) => {
+    const ageRangeFromFace = data.FaceDetails[0].AgeRange;
+    const low = ageRangeFromFace.Low;
+    const high = ageRangeFromFace.High;
 
-//     //TODO : 나이 output 내는 방식 .. 평균내는 것 말고
-//     return parseInt(low+high / 2);
-// }
+    //TODO : 나이 output 내는 방식 .. 평균내는 것 말고
+    return parseInt(low+high / 2);
+}
 
 module.exports = router;
