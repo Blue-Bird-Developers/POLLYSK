19e043c64032833ea6256cfc5adc9ac05e49dbf2
juhyeon96 <kjh96905@gmail.com>
2019-11-20 18:31:18 +0900

    aws rekognition error 다 고침 ㅎㅎ

--- server/routes/api/rekognition/rekognition.js
+++ server/routes/api/rekognition/rekognition.js
@@ -8,9 +8,10 @@ AWS.config.loadFromPath(__dirname + '/rekoConfig.json');
 const User = require('../../../model/User')
 const rekognition = new AWS.Rekognition();
 
+
 router.post('/:userId', (req, res) => {
     const userId = req.params.userId;
-    var faceSingle = '';
+    var faceSingle = "";
 
     if (!userId) {
         res.status(code.BAD_REQUEST)
@@ -19,67 +20,75 @@ router.post('/:userId', (req, res) => {
     }
 
     User.read({
-            userId
-        })
-        .then(face => {
-            faceSingle = face.face;
-            return {
-                code: code.OK,
-                json: util.successTrue('', faceSingle)
-            };
-        }).catch(err => {
-            console.log(err);
-            return {
-                code: code.INTERNAL_SERVER_ERROR,
-                json: util.successFalse(msg.INTERNAL_SERVER_ERROR)
-            };
-        })
+        userId
+    })
+    .then(face => {
+        faceSingle = face.face;
 
-    /*rekognition의 detectText 함수에 필요한 parameter 변수입니다.
-    constparams={
-    Image:{
-    Bytes:faceSingle
-    }
-    }*/
-
-    constimageURL = faceSingle
-    //constresponseImage=axios.get(
-    //imageURL,
-    //{responseType:’arraybuffer’}
-    //)
-
-    constparams = {
-        Image: {
-            Bytes: newBuffer.from(imageURL, 'binary')
+        return {
+            code: code.OK,
+            json: util.successTrue('', faceSingle)
+        };
+    }).catch(err => {
+        console.log(err);
+        return {
+            code: code.INTERNAL_SERVER_ERROR,
+            json: util.successFalse(msg.INTERNAL_SERVER_ERROR)
+        };
+    })
+    .then(faceSingle => {
+        const imageURL = faceSingle.json.data;
+        const fileName = imageURL.substr(imageURL.lastIndexOf('/') + 1);
+        
+        return fileName;
+    })
+    .then(input => {
+        const params = {
+            "Image": {
+                "S3Object": {
+                    "Bucket": "rekogtest12",
+                    "Name": input
+                }
+            },
+            "Attributes": [
+                "ALL"
+            ]
         }
-    }
 
-    rekognition.detectFaces(params, (error, data) => {
-        if (error) {
-            res.send(error);
-        } else {
-            if (data.FaceDetails && data.FaceDetails.length > 0) {
-                const detectedAge = {
-                    age: detectUserAgeRange(data)
-                };
-                res.json(detectedAge);
+        return params;
+    })
+    .then(params => {
+        rekognition.detectFaces(params, (error, data) => {
+            if (error) {
+                res.send(error);
             } else {
-                const invalidAge = {
-                    age: "none"
-                };
-                res.json(invalidAge);
+
+                if (data.FaceDetails && data.FaceDetails.length > 0) {
+                    const detectedAge = {
+                        age: detectUserAgeRange(data)
+                    };
+
+                    res.json(detectedAge);
+                } else {
+                    const invalidAge = {
+                        age: "none"
+                    };
+
+                    res.json(invalidAge);
+                }
+
             }
-        }
-    });
+        });
+    })
 });
 
 const detectUserAgeRange = (data) => {
     const ageRangeFromFace = data.FaceDetails[0].AgeRange;
     const low = ageRangeFromFace.Low;
     const high = ageRangeFromFace.High;
-
+    
     //TODO:나이output내는방식..평균내는것말고
-    return parseInt(low + high / 2);
+    return parseInt((low + high) / 2);
 }
 
 module.exports = router;
