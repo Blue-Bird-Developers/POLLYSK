23011134c6678c4be1ad78f7022bf2c6d50ff923
Seohyun Yoon <sarahseohyun@gmail.com>
2019-11-21 19:24:44 +0900

    Menu Component Added

--- client/src/views/SelectMenu.tsx
+++ client/src/views/SelectMenu.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 import styled from 'styled-components'
 import { Store } from '../store'
 import { observer, inject } from 'mobx-react'
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
+import Menus from './Menu'
 
 @inject('store')
 @observer
@@ -13,6 +14,7 @@ export default class SelectMenu extends React.Component {
     return (
       <Container>
         <Title>주문페이지입니다.</Title>
+        <Menus />
         <Link to='/pay'>
           <OrderButton onClick={this.onClickOrderButton}>주문하기</OrderButton>
         </Link>
