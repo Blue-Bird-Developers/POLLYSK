3f636c7e0fc520f851aade133500a556495d57ad
shindongsun0 <shindongsun0@naver.com>
2019-11-21 21:03:04 +0900

    'getPollyFromS3_update1'

--- server/routes/api/polly/polly.js
+++ server/routes/api/polly/polly.js
@@ -6,38 +6,32 @@ const AWS = require('aws-sdk');
 AWS.config.loadFromPath(__dirname + '/pollyConfig.json');
 
 router.get('/:pollyName', (req, res) => {
-    console.log(req.query);
+    const pollyName = 
+    console.log(pollyName);
     const s3 = new AWS.S3();
     const params = {
         Bucket: "pollybucket223",
         Key: `${pollyName}.mp3`,
     };
-    s3.getSignedUrl('getObject', getParams, function(err, url) {
+    s3.getSignedUrl('getObject', params, function(err, url) {
         if(err) { throw {msg:err, code:"AWS_ERROR"}; }
         else {
             return url;
         }
     });
 })
-
-// //const s3 = new AWS.S3();
-// router.post('/:pollyId', (req, res) => {
-//     // const params = {
-//     //     Bucket: 'pollybucket223',
-//     //     Key: 'milkteaPolly.mp3'
-//     // };
-//     const pollyId = req.params.pollyId;
-//     const getParams = {
-//         Bucket: "pollybucket223",
-//         Key: `${pollyId}.mp3`
-//     };
-    
-//     s3.getSignedUrl('getObject', getParams, function(err, url) {
-//         if(err) { throw {msg:err, code:"AWS_ERROR"}; }
-//         else {
-//             return url;
-//         }
-//     });
+// const pollyName = "teaPolly";
+// const s3 = new AWS.S3();
+// const params = {
+//     Bucket: "pollybucket223",
+//     Key: `${pollyName}.mp3`,
+// };
+// s3.getSignedUrl('getObject', params, function(err, url) {
+//     if(err) { throw {msg:err, code:"AWS_ERROR"}; }
+//     else {
+//         console.log(JSON.stringify(url));
+//         return JSON.stringify(url);
+//     }
 // });
 
 module.exports = router;
