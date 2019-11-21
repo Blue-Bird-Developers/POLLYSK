ce08e1b231eeeaccaa920be1d68bf90c8cafc983
siyeons <siyeonleeme@gmail.com>
2019-11-21 20:51:39 +0900

    Update 기능 추가

--- server/model/User.js
+++ server/model/User.js
@@ -1,20 +1,32 @@
 const pool = require('../module/poolAsync');
 
 module.exports = {
-    read: ({userId}) => {
-        const table = 'users';
-        const query = `SELECT * FROM ${table} WHERE userId = ${userId}`;
-        return pool.queryParam_None(query)
-        .then(result => {
-            const face = result[0];
-            if(!face){
-                throw new Error('No Face');
-            }
-            return face;
-        })
-        .catch(err => {
-            console.log(err);
-            throw err;
-        })
-    },
-};
+
+        read: ({
+            userId
+        }) => {
+            const table = 'users';
+            const query = `SELECT * FROM ${table} WHERE userId = ${userId}`;
+            return pool.queryParam_None(query)
+                .then(result => {
+                    const face = result[0];
+                    if (!face) {
+                        throw new Error('No Face');
+                    }
+                    return face;
+                })
+                .catch(err => {
+                    console.log(err);
+                    throw err;
+                })
+        },
+
+        update: ({
+            userId,
+            age
+        }) => {
+            const table = 'users';
+            console.log(age);
+            return pool.queryParam_None(`UPDATE ${table} SET age = ${age} WHERE userId = ${userId}`)      
+        }
+}
