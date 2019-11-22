18ff997f210e7c11b2b88c1144a2e4f7f6958eb0
siyeons <siyeonleeme@gmail.com>
2019-11-23 08:12:20 +0900

    메세지 수정

--- server/module/responseMessage.js
+++ server/module/responseMessage.js
@@ -3,8 +3,10 @@ module.exports = {
     NULL_VALUE: "필요한 값이 없습니다.",
     OUT_OF_VALUE: "파라미터 값이 잘못 되었습니다.",
 
-    FACE_READ_SUCCESS: "조회 성공",
-    FACE_READ_FAIL: "조회 성공",
+    FACE_READ_SUCCESS: "사진 조회 성공",
+    FACE_READ_FAIL: "사진 조회 실패",
+
+    ID_READ_SUCCESS: "아이디 조회 성공",
 
     AGE_UPDATE_SUCCESS: "나이 입력 성공",
 
