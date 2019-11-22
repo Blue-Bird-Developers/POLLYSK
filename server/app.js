b38292f201033ff9892c50a721c4cb68e78d0eba
siyeons <siyeonleeme@gmail.com>
2019-11-22 22:12:05 +0900

    bodyparser 추가

--- server/app.js
+++ server/app.js
@@ -19,6 +19,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// for base64 Image
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+
+
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
